f33b205812f940baa22260f07e9f8a9064241c62
zichen <zichen1016@foxmail.com>
2021-07-12 00:22:55 +0800

    Servlet的实现类HttpServlet类

---
+++ 202107101859/demo02/src/com/zichen/web/servlet/ServletDemo03.java
@@ -0,0 +1,30 @@
+package com.zichen.web.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+/**
+ * 使用 Servlet 的实现类 HttpServlet
+ * HttpServlet 实现了 service 方法 并判断网络请求方式 创建了 doGet、doPost、doPut等方法
+ * @author zc
+ * @date 2021-07-12 0:11
+ */
+public class ServletDemo03 extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        System.out.println("doGet...");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+        System.out.println("doPost...");
+    }
+
+}
