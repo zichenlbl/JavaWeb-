2b2bdde8b53e315ed499da3710a28335c75d6839
zichen <zichen1016@foxmail.com>
2021-07-17 20:19:06 +0800

    cookie中存中文数据

--- 202107101859/demo02/src/com/zichen/web/cookie/CookieDemo01.java
+++ 202107101859/demo02/src/com/zichen/web/cookie/CookieDemo01.java
@@ -8,9 +8,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URLEncoder;
 
 /**
  * Cookie 快速入门
+ * cookie中存中文数据
  * @author zc
  * @date 2021-07-17 18:57
  */
@@ -18,10 +20,16 @@ import java.io.IOException;
 public class CookieDemo01 extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // 1.创建Cookie对象
+        // cookie中存中文 URL编码 Tomcat 8 版本以下不能直接存中文数据
+        String name = URLEncoder.encode("张三", "utf-8");
+        // 1.创建Cookie对象 可以创建多个Cookie 浏览器关闭后默认Cookie删除
         Cookie cookie = new Cookie("msg", "hello");
+        Cookie cookie1 = new Cookie("code", name);
+        // 设置Cookie保存在本地的时间 负数：默认 正数：存活时间(秒) 零：删除cookie参数
+        cookie.setMaxAge(60);
         // 2.发送Cookie
         resp.addCookie(cookie);
+        resp.addCookie(cookie1);
     }
 
     @Override
--- 202107101859/demo02/src/com/zichen/web/cookie/CookieDemo02.java
+++ 202107101859/demo02/src/com/zichen/web/cookie/CookieDemo02.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 
 /**
  * Cookie 快速入门
@@ -20,11 +21,13 @@ public class CookieDemo02 extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // 3.获取Cookie
         Cookie[] cookies = req.getCookies();
-        // 获取数据，遍历Cookies
+        // 获取数据，遍历Cookies 中文解码
         if (cookies != null) {
             for (Cookie cookie : cookies) {
                 String name = cookie.getName();
                 String value = cookie.getValue();
+                name = URLDecoder.decode(name, "utf-8");
+                value = URLDecoder.decode(value, "utf-8");
                 System.out.println(name + ":" + value);
             }
         }
