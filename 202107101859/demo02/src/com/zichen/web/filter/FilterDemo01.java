b2cf605b3ef3a4f750337607294dc268cb16d1ab
zichen <zichen1016@foxmail.com>
2021-07-18 20:23:21 +0800

    Filter过滤器执行流程

--- 202107101859/demo02/src/com/zichen/web/filter/FilterDemo01.java
+++ 202107101859/demo02/src/com/zichen/web/filter/FilterDemo01.java
@@ -13,20 +13,37 @@ import java.io.IOException;
  */
 @WebFilter("/*")
 public class FilterDemo01 implements Filter {
+    /**
+     * 在服务器启动后，会创建Filter对象，然后调用init方法。只执行一次 用于加载资源
+     * @param  filterConfig filterConfig
+     * @throws ServletException ServletException
+     */
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        System.out.println("init...");
     }
 
+    /**
+     * 每一次请求被拦截资源时，会执行 执行多次
+     * @param servletRequest servletRequest
+     * @param servletResponse servletResponse
+     * @param filterChain filterChain
+     * @throws IOException IOException
+     * @throws ServletException ServletException
+     */
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         System.out.println("doFilter...");
         // 放行
         filterChain.doFilter(servletRequest, servletResponse);
+        System.out.println("doFilter1...");
     }
 
+    /**
+     * 在服务器关闭后，Filter对象被销毁。如果服务器是正常关闭，则会执行destroy方法。只执行一次 用于释放资源
+     */
     @Override
     public void destroy() {
-
+        System.out.println("destroy...");
     }
 }
