aa01d298c2ae80bbf488ba323321ab9a99b51ef0
zichen <zichen1016@foxmail.com>
2021-07-28 18:39:46 +0800

    实现线程的第二种方式-使用匿名内部类

--- 202107101859/demo04/src/com/zichen/javase/multithreading/ThreadDemo02.java
+++ 202107101859/demo04/src/com/zichen/javase/multithreading/ThreadDemo02.java
@@ -8,8 +8,18 @@ package com.zichen.javase.multithreading;
 public class ThreadDemo02 {
 
     public static void main(String[] args) {
+        // 实现线程的第二种方式
         MyRunnable myRunnable = new MyRunnable();
-        Thread thread = new Thread(myRunnable);
+        Thread thread/* = new Thread(myRunnable)*/;
+        // 使用匿名内部类的方式
+        thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    System.out.println("分支线程--->" + i);
+                }
+            }
+        });
         thread.start();
         for (int i = 0; i < 1000; i++) {
             System.out.println("主线程--->" + i);
